190dbda2eacfc80c2d6a546a1e53fb94850bf5fd
contributorsambhav <sambhav511974@gmail.com>
2024-06-27 18:14:08 +0530

    created initial look of game mode selector component

--- frontend/chess/src/App.jsx
+++ frontend/chess/src/App.jsx
@@ -4,19 +4,27 @@ import Navbar from './components/Navbar';
 import Login from './components/Login';
 import Home from './components/Home';
 import SignUp from './components/SignUp';
+import ChessboardComponent from './components/ChessBoard';
+import GameModeSelector from './components/GameModeSelector';
+
 function App() {
-    return (
-        <Router>
-            <div>
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/login" element={<Login />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/modeselector" element={<GameModeSelector />} />
+            <Route path="/local-multiplayer" element={<ChessboardComponent />} />
+
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
