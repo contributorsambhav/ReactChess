701b1d4169df9109875af0ca27680cbf311bc701
contributorsambhav <sambhav511974@gmail.com>
2024-07-25 23:50:56 +0530

    new scheme of styling board component

--- frontend/chess/src/components/Modes/LocalMultiplayer.jsx
+++ frontend/chess/src/components/Modes/LocalMultiplayer.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Chess } from 'chess.js';
-import Chessboard from 'chessboardjs';
-import axios from 'axios';
-import { Howl } from 'howler';
-import moveSoundFile from '../../assets/sounds/move.mp3';
-import captureSoundFile from '../../assets/sounds/capture.mp3';
-import checkSoundFile from '../../assets/sounds/check.mp3';
-import checkmateSoundFile from '../../assets/sounds/checkmate.mp3';
+import React, { useEffect, useRef, useState } from "react";
+import { Chess } from "chess.js";
+import Chessboard from "chessboardjs";
+import { Howl } from "howler";
+import moveSoundFile from "../../assets/sounds/move.mp3";
+import captureSoundFile from "../../assets/sounds/capture.mp3";
+import checkSoundFile from "../../assets/sounds/check.mp3";
+import checkmateSoundFile from "../../assets/sounds/checkmate.mp3";
 import pieceImages from "../pieceImages";
-import boardbg from '../../assets/images/bgboard.jpg';
+import boardbg from "../../assets/images/bgboard.jpg";
 
 const moveSound = new Howl({ src: [moveSoundFile] });
 const captureSound = new Howl({ src: [captureSoundFile] });
@@ -26,29 +25,13 @@ const debounce = (func, delay) => {
 };
 
 const LocalMultiplayer = () => {
-  const fetchBestMove = async (FEN) => {
-    try {
-      const response = await axios.get('http://localhost:8123/stockfish', {
-        params: {
-          fen: FEN,
-          depth: 10
-        }
-      });
-      console.log('Response from server:', response.data);
-      return response.data.bestMove;
-    } catch (error) {
-      console.error('Error fetching move from stockfish:', error);
-      return null;
-    }
-  };
-
   const chessRef = useRef(null);
   const boardRef = useRef(null);
   const [currentStatus, setCurrentStatus] = useState(null);
   const [moves, setMoves] = useState([]);
   const gameRef = useRef(new Chess());
-  const [isTableCollapsed, setIsTableCollapsed] = useState(true);
-  const [promotionPiece, setPromotionPiece] = useState('q');
+  const [isTableCollapsed, setIsTableCollapsed] = useState(false);
+  const [promotionPiece, setPromotionPiece] = useState("q");
 
   useEffect(() => {
     const game = gameRef.current;
@@ -59,8 +42,10 @@ const LocalMultiplayer = () => {
         return false;
       }
 
-      if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-        (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+      if (
+        (game.turn() === "w" && piece.search(/^b/) !== -1) ||
+        (game.turn() === "b" && piece.search(/^w/) !== -1)
+      ) {
         return false;
       }
     };
@@ -71,12 +56,12 @@ const LocalMultiplayer = () => {
       let move = game.move({
         from: source,
         to: target,
-        promotion: promotionPiece // Use the selected promotion piece
+        promotion: promotionPiece, // Use the selected promotion piece
       });
 
       if (move === null) return "snapback";
 
-      setMoves(prevMoves => [...prevMoves, { from: move.from, to: move.to }]);
+      setMoves((prevMoves) => [...prevMoves, { from: move.from, to: move.to }]);
       updateStatus();
 
       // Play sound based on move type
@@ -90,7 +75,7 @@ const LocalMultiplayer = () => {
     const onMouseoverSquare = (square, piece) => {
       const moves = game.moves({
         square: square,
-        verbose: true
+        verbose: true,
       });
 
       if (moves.length === 0) return;
@@ -111,40 +96,40 @@ const LocalMultiplayer = () => {
     };
 
     const updateStatus = debounce(() => {
-      let status = '';
-      let moveColor = 'White';
+      let status = "";
+      let moveColor = "White";
 
-      if (game.turn() === 'b') {
-        moveColor = 'Black';
+      if (game.turn() === "b") {
+        moveColor = "Black";
       }
 
       if (game.isGameOver()) {
-        status = 'Game over';
+        status = "Game over";
       } else {
-        status = moveColor + ' to move';
+        status = moveColor + " to move";
 
         if (game.isCheckmate()) {
-          status += ', ' + moveColor + ' is in checkmate';
+          status += ", " + moveColor + " is in checkmate";
           checkmateSound.play();
         } else if (game.inCheck()) {
-          status += ', ' + moveColor + ' is in check';
+          status += ", " + moveColor + " is in check";
           checkSound.play();
         }
       }
 
       setCurrentStatus(status);
     }, 100);
-    
+
     const removeGreySquares = () => {
-      const squares = document.querySelectorAll('.square-55d63');
-      squares.forEach(square => square.style.background = '');
+      const squares = document.querySelectorAll(".square-55d63");
+      squares.forEach((square) => (square.style.background = ""));
     };
 
     const greySquare = (square) => {
       const squareEl = document.querySelector(`.square-${square}`);
       if (squareEl) {
-        const isBlack = squareEl.classList.contains('black-3c85d');
-        squareEl.style.background = isBlack ? '#696969' : '#a9a9a9';
+        const isBlack = squareEl.classList.contains("black-3c85d");
+        squareEl.style.background = isBlack ? "#696969" : "#a9a9a9";
       }
     };
 
@@ -158,7 +143,7 @@ const LocalMultiplayer = () => {
       onSnapEnd: onSnapEnd,
       pieceTheme: (piece) => pieceImages[piece],
       snapbackSpeed: 500,
-      snapSpeed: 100
+      snapSpeed: 100,
     };
 
     boardRef.current = Chessboard(chessRef.current, config);
@@ -179,47 +164,106 @@ const LocalMultiplayer = () => {
   };
 
   return (
-    <div className="flex h-screen items-center justify-center w-screen"
-         style={{ backgroundImage: `url(${boardbg})`, backgroundSize: 'cover' }}>
-      <div className='w-screen flex flex-col md:flex-row mx-auto my-auto'>
-        <div className='mx-16 w-full md:w-1/2'>
-          <div ref={chessRef} style={{ width: window.innerWidth > 1536 ? '40vw' : '70vw' }}></div>
+    <div
+      className="flex h-screen items-center justify-center w-screen"
+      style={{ backgroundImage: `url(${boardbg})`, backgroundSize: "cover" }}
+    >
+      <div className="w-screen flex flex-col md:flex-row mx-auto my-auto">
+        <div className="mx-16 w-full md:w-1/2">
+          <div
+            ref={chessRef}
+            style={{ width: window.innerWidth > 1536 ? "40vw" : "70vw" }}
+          ></div>
         </div>
-        <div className='ml-4 w-full md:w-1/3 mt-4 md:mt-0'>
-          <div className='rounded-xl text-center p-8 px-16 w-full text-4xl bg-gray-400 bg-opacity-30 text-white border border-white flex-shrink-0'>
+        <div className="ml-4 w-full md:w-1/3 mt-4 md:mt-0">
+          <div className="rounded-xl shadow-lg text-center p-8 px-16 w-full text-4xl bg-gray-400 bg-opacity-30 text-white border border-gray-200 flex-shrink-0">
             Current Status: {currentStatus ? currentStatus : "White to move"}
           </div>
-          <div className='mt-4'>
-            <label className='mr-2 text-white text-xl'>Promotion Piece:</label>
-            <select value={promotionPiece} onChange={handlePromotionChange} className='bg-gray-400 bg-opacity-30 text-white px-4 py-2 rounded-lg w-full text-lg'>
-              <option className="bg-blue-900 bg-opacity-50 bg-transparent text-white" value="q">Queen</option>
-              <option className="bg-blue-900 bg-opacity-50 bg-transparent text-white" value="r">Rook</option>
-              <option className="bg-blue-900 bg-opacity-50 bg-transparent text-white" value="b">Bishop</option>
-              <option className="bg-blue-900 bg-opacity-50 bg-transparent text-white" value="n">Knight</option>
+
+          <div className="mt-4">
+            <label className="mr-2 text-white text-xl">Promotion Piece:</label>
+            <select
+              value={promotionPiece}
+              onChange={handlePromotionChange}
+              className="bg-gray-400 bg-opacity-30 text-white px-4 py-2 rounded-lg w-full text-lg"
+            >
+              <option
+                className="bg-blue-900 bg-opacity-50 bg-transparent text-white"
+                value="q"
+              >
+                Queen
+              </option>
+              <option
+                className="bg-blue-900 bg-opacity-50 bg-transparent text-white"
+                value="r"
+              >
+                Rook
+              </option>
+              <option
+                className="bg-blue-900 bg-opacity-50 bg-transparent text-white"
+                value="b"
+              >
+                Bishop
+              </option>
+              <option
+                className="bg-blue-900 bg-opacity-50 bg-transparent text-white"
+                value="n"
+              >
+                Knight
+              </option>
             </select>
           </div>
-          <div className='mx-2 mt-3 text-center text-2xl text-white bg-black bg-opacity-20 p-4 rounded-lg'>
-            If the game goes to start after promotion piece change, just attempt an illegal move, it will get OK so relax
+          <div className="mx-2 mt-3 text-center border border-gray-800 text-2xl text-white bg-black bg-opacity-20 p-4 rounded-lg">
+            If the game goes to start after promotion piece change, just attempt
+            an illegal move, it will get OK so relax
           </div>
-          <button onClick={toggleTable} className='mt-4 bg-gray-400 bg-opacity-30 text-white border border-white px-6 py-3 rounded-lg w-full text-xl'>
-            {isTableCollapsed ? 'Show Moves' : 'Hide Moves'}
+          <button
+            onClick={toggleTable}
+            className="mt-4 bg-gray-400 bg-opacity-30 text-white border border-gray-200 px-6 py-3 rounded-lg w-full text-xl"
+          >
+            {isTableCollapsed ? "Show Moves" : "Hide Moves"}
           </button>
-          <div style={{ maxHeight: isTableCollapsed ? '0' : '40vh', transition: 'max-height 0.3s ease-in-out', overflow: 'scroll' }}>
-            <div style={{ height: '100%', overflowY: 'auto' }}>
-              <table className='w-full border-collapse border border-gray-700 rounded-lg bg-gray-400 bg-opacity-30 text-white'>
+          <div
+            style={{
+              maxHeight: isTableCollapsed ? "0" : "40vh",
+              transition: "max-height 0.3s ease-in-out",
+              overflow: "scroll",
+            }}
+          >
+            <div style={{ height: "100%", overflowY: "auto" }}>
+              <table className="w-full border-collapse border border-gray-700 rounded-lg bg-gray-400 bg-opacity-30 text-white">
                 <thead>
-                  <tr className='bg-gray-800 bg-opacity-30 text-center text-white'>
-                    <th className='border border-white px-6 py-3 text-xl'>Move</th>
-                    <th className='border border-white px-6 py-3 text-xl'>From</th>
-                    <th className='border border-white px-6 py-3 text-xl'>To</th>
+                  <tr className="bg-gray-800 bg-opacity-30 text-center text-white">
+                    <th className="border border-gray-400 px-6 py-3 text-xl">
+                      Move
+                    </th>
+                    <th className="border border-gray-400 px-6 py-3 text-xl">
+                      From
+                    </th>
+                    <th className="border border-gray-400 px-6 py-3 text-xl">
+                      To
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
                   {moves.map((move, index) => (
-                    <tr key={index} className={index % 2 === 0 ? 'bg-gray-700 bg-opacity-30 text-white text-center' : 'bg-gray-600 bg-opacity-30 text-gray-200 text-center'}>
-                      <td className='border border-white px-6 py-4 text-xl'>{index + 1}</td>
-                      <td className='border border-white px-6 py-4 text-xl'>{move.from}</td>
-                      <td className='border border-white px-6 py-4 text-xl'>{move.to}</td>
+                    <tr
+                      key={index}
+                      className={
+                        index % 2 === 0
+                          ? "bg-gray-700 bg-opacity-30 text-white text-center"
+                          : "bg-gray-600 bg-opacity-30 text-gray-200 text-center"
+                      }
+                    >
+                      <td className="border border-gray-400 px-6 py-4 text-xl">
+                        {index + 1}
+                      </td>
+                      <td className="border border-gray-400 px-6 py-4 text-xl">
+                        {move.from}
+                      </td>
+                      <td className="border border-gray-400 px-6 py-4 text-xl">
+                        {move.to}
+                      </td>
                     </tr>
                   ))}
                 </tbody>
