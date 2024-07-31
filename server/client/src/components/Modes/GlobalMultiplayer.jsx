ebb5bfab2c45a5354e91748493d14f952e5e852b
contributorsambhav <sambhav511974@gmail.com>
2024-07-31 23:18:21 +0530

    ..

--- server/client/src/components/Modes/GlobalMultiplayer.jsx
+++ server/client/src/components/Modes/GlobalMultiplayer.jsx
@@ -265,7 +265,7 @@ const GlobalMultiplayer = () => {
         <WaitQueue />
       ) : (
         <div
-          className={`flex min-h-screen overflow-scroll items-center justify-center w-screen`}
+          className={`flex min-h-screen ${mobileMode ? "h-[80vh]" : ""} overflow-scroll items-center justify-center w-screen`}
           style={{
             backgroundImage: `url(${boardbg})`,
             backgroundSize: "cover",
